beb59ca33fa6ff73be725c69f9790be6e0424b62
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-04 21:00:05 +0000

    Updated the user model

--- reko/social/reko/admin.py
+++ reko/social/reko/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group, User
 
-# Register your models here.
+# Unregister "Groups"
+admin.site.unregister(Group)
+
+# Extend User Model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    # Display just username fields on "admin" page
+    fields = ["username"]
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- reko/social/reko/models.py
+++ reko/social/reko/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Let's create a user profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # We'll associate one user to one profile, and if User gets deleted, delete "profile" too
+    follows = models.ManyToManyField("self",
+    related_name="followed_by",
+    symmetrical=False,
+    blank=True)
