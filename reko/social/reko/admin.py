5f08dd883681b1658815ddbbc1de9360cc1ed590
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-04 21:36:45 +0000

    Following system done! Now it displays from the Admin Panel

--- reko/social/reko/admin.py
+++ reko/social/reko/admin.py
@@ -5,15 +5,21 @@ from .models import Profile
 # Unregister "Groups"
 admin.site.unregister(Group)
 
+# mix profile info into User info
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
 # Extend User Model
 class UserAdmin(admin.ModelAdmin):
     model = User
     # Display just username fields on "admin" page
     fields = ["username"]
+    inlines = [ProfileInline]
 
 #Unregister initial user
 admin.site.unregister(User)
 
 #re-register User and Profile
 admin.site.register(User, UserAdmin)
-admin.site.register(Profile)
+# admin.site.register(Profile)
+
