c823fc418d3a0628332259644cbd7a54727adf43
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-04 21:49:13 +0000

    Add user profile creation on user signup

--- reko/social/reko/models.py
+++ reko/social/reko/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 # Let's create a user profile model
 class Profile(models.Model):
@@ -11,3 +12,12 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+# Create a Profile when a new user signs up
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        user_profile = Profile(user=instance)
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User) # When a new user is created, call create_profile
+# https://youtu.be/H8MmNqDyra8?si=lp9AVRKHJp6sJkVv&t=303
