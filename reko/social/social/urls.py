38bbde28ca4a0c50067abf7f6145b5e9c1cdf63e
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-04 20:00:14 +0000

    Finishing urls.py

--- reko/social/social/urls.py
+++ reko/social/social/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.home, name="home"),
+    path("admin/", admin.site.urls),
+    path("", include("reko.urls")),
 ]
