69ed37378567da72ffbd1f74ae2dc7f5155648c4
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-04 21:01:30 +0000

    Commenting models.py

--- reko/social/reko/models.py
+++ reko/social/reko/models.py
@@ -5,6 +5,6 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE) # We'll associate one user to one profile, and if User gets deleted, delete "profile" too
     follows = models.ManyToManyField("self",
-    related_name="followed_by",
-    symmetrical=False,
-    blank=True)
+    related_name="followed_by", # Ppl who follows you
+    symmetrical=False, # If you follow someone, that someone doesn't have to follow you, right?
+    blank=True) # I mean it would be cool if you started following me... <3 see ya there boi / Yeah but you start following nothing...
