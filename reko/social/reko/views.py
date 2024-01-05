bcee3db9657d34439d9047c26b468ffaa1c195c2
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-05 17:17:29 +0000

    Update profile_list.html and views.py

--- reko/social/reko/views.py
+++ reko/social/reko/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import Profile
 
 def home(request):
     return render(request, 'home.html', {})
 
 def profile_list(request):
-    profiles = Profile.objects.exclude(user=request.user)
-    return render(request, 'profile_list.html', {"profiles": profiles})
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        return render(request, 'profile_list.html', {"profiles": profiles})
+    else:
+        messages.success(request, ('You must to be logged in before trying to make new friends, dude!'))
+        return redirect('home')
