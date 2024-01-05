de8959417cbf19e00e1c2f823d5bc75fb7615dbe
Luca Tamagnini <151396032+MrDaPoyo@users.noreply.github.com>
2024-01-05 18:14:36 +0000

    Add profile view and update error message

--- reko/social/reko/views.py
+++ reko/social/reko/views.py
@@ -10,5 +10,14 @@ def profile_list(request):
         profiles = Profile.objects.exclude(user=request.user)
         return render(request, 'profile_list.html', {"profiles": profiles})
     else:
-        messages.success(request, ('You must to be logged in before trying to view other profiles.'))
+        messages.success(request, ('You must log-in in order to view other profiles.'))
         return redirect('home')
+    
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        return render(request, 'profile.html', {"profile": profile})
+    else:
+        messages.success(request, ('You must log-in in order to view other profiles.'))
+        return redirect('home')
+   
